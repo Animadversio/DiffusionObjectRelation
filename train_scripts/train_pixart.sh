41bdab06f6d7eaf59b5492e907e619e293537447
Binxu Wang <binxu_wang@hms.harvard.edu>
2025-07-17 01:48:55 -0400

    add new training runs

--- train_scripts/train_pixart.sh
+++ train_scripts/train_pixart.sh
@@ -96,3 +96,22 @@ torchrun --nproc_per_node=1 \
     --report_to "tensorboard" \
     --loss_report_name "train_loss"
 
+
+# train conditional diffusion with random embedding, with positional embedding 
+cd ~/Github/DiffusionObjectRelation
+torchrun --nproc_per_node=1 \
+    PixArt-alpha/train_scripts/train_with_visualize.py \
+    /n/home12/binxuwang/Github/DiffusionObjectRelation/train_scripts/train_configs/PixArt_mini_img128_internal_objrelation_T5_prompt20_training_from_scratch.py \
+    --work-dir $STORE_DIR"/DL_Projects/PixArt/results/objrel_T5_DiT_mini_pilot/" \
+    --report_to "tensorboard" \
+    --loss_report_name "train_loss"
+
+
+cd ~/Github/DiffusionObjectRelation
+torchrun --nproc_per_node=1 \
+    PixArt-alpha/train_scripts/train_with_visualize.py \
+    /n/home12/binxuwang/Github/DiffusionObjectRelation/train_scripts/train_configs/PixArt_B_img128_internal_objrelation_T5_prompt20_training_from_scratch.py \
+    --work-dir $STORE_DIR"/DL_Projects/PixArt/results/objrel_T5_DiT_B_pilot/" \
+    --report_to "tensorboard" \
+    --loss_report_name "train_loss"
+
